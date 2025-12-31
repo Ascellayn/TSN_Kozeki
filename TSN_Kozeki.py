d73bed11f598ae47d1484050f7287cef70eabc8a
Ascellayn <root.siriusb@gmail.com>
2025-12-31 03:31:14 +0100

    BF¤Extractor | Separate extractions by molru file

--- TSN_Kozeki.py
+++ TSN_Kozeki.py
@@ -2,17 +2,17 @@ from TSN_Abstracter import *;
 import sys;
 
 Log.Clear();
-Kozeki_Version: str = "v0.2.0";
+Kozeki_Version: str = "v0.2.1";
 Kozeki_Branch: str = "Azure";
 
 
 
-def Extract_Slow(F: str, Path: str) -> None:
+def Extract_Slow(F: str) -> None:
 	""" Byte by Byte extraction, reveals data that is currently unknown by TSN_Kozeki  
 	Warning: Painfully slow and to not be used! """
 	Molru_Init: int = Time.Get_Unix(True);
-	File.Path_Require(f"Extracted/{Path}");
-	File.Path_Require(f"Unknown/{Path}");
+	File.Path_Require(f"Extracted/{F}");
+	File.Path_Require(f"Unknown/{F}");
 
 	with open(F, "r+b") as Molru:
 		Bytes: bytes = Molru.read();
@@ -45,7 +45,7 @@ def Extract_Slow(F: str, Path: str) -> None:
 			# Finish JPEG
 		if (OxFF and JPEG_End):
 			Buffer_JPEG += Byte; JPEG_Data = False;
-			with open(f"Extracted/{Path}/{Index}.jpg", "w+b") as JPEG: JPEG.write(Buffer_JPEG); Buffer_JPEG = b"";
+			with open(f"Extracted/{F}/{Index}.jpg", "w+b") as JPEG: JPEG.write(Buffer_JPEG); Buffer_JPEG = b"";
 			Log.Stateless(f"Writing JPEG → {Index}/{Bytes_Total} ({round((Index/Bytes_Total)*100, 2)}%) Bytes");
 			JPEG_Data = False;
 
@@ -55,7 +55,7 @@ def Extract_Slow(F: str, Path: str) -> None:
 
 			if (len(Buffer_Unknown) > 1):
 				Buffer_Unknown[:-1];
-				with open(f"Unknown/{Path}/{Index}.hex", "w+b") as Hex: Hex.write(Buffer_Unknown);
+				with open(f"Unknown/{F}/{Index}.hex", "w+b") as Hex: Hex.write(Buffer_Unknown);
 				Log.Stateless(f"Writing Hex → {Index}/{Bytes_Total} ({round((Index/Bytes_Total)*100, 2)}%) Bytes");
 			Buffer_Unknown = b"";
 
@@ -71,11 +71,11 @@ def Extract_Slow(F: str, Path: str) -> None:
 
 
 
-def Extract_Deep(F: str, Path: str) -> None:
+def Extract_Deep(F: str) -> None:
 	""" Byte by Byte extraction, may reveal data that is currently unknown by TSN_Kozeki """
 	Molru_Init: int = Time.Get_Unix(True);
-	File.Path_Require(f"Unknown/{Path}");
-	File.Path_Require(f"Extracted/{Path}");
+	File.Path_Require(f"Unknown/{F}");
+	File.Path_Require(f"Extracted/{F}");
 
 	with open(F, "r+b") as Molru: Bytes: bytes = Molru.read();
 	Offset: int = 0;
@@ -86,10 +86,10 @@ def Extract_Deep(F: str, Path: str) -> None:
 		if (JPEG_End == -1): break;
 
 		if (JPEG_Start != 0):
-			with open(f"Unknown/{Path}/0x{Offset}-0x{Offset + JPEG_Start}.hex", "w+b") as Hex: Hex.write(Bytes[:JPEG_Start]);
+			with open(f"Unknown/{F}/0x{Offset}-0x{Offset + JPEG_Start}.hex", "w+b") as Hex: Hex.write(Bytes[:JPEG_Start]);
 			Log.Info("Unknown data found before JPEG File, writing.");
 
-		with open(f"Extracted/{Path}/0x{Offset + JPEG_Start}-0x{Offset + JPEG_End}.jpg", "w+b") as Hex: Hex.write(Bytes[JPEG_Start:JPEG_End]);
+		with open(f"Extracted/{F}/0x{Offset + JPEG_Start}-0x{Offset + JPEG_End}.jpg", "w+b") as Hex: Hex.write(Bytes[JPEG_Start:JPEG_End]);
 		Offset += JPEG_End + 2; Bytes = Bytes[JPEG_End:];
 		
 		Log.Info(f"JPEG File of {JPEG_End - JPEG_Start} bytes in size written. {len(Bytes)} Bytes left to process.");
@@ -114,8 +114,8 @@ def Kozeki(Extractor: str) -> None:
 				if (File.endswith(".molru")):
 					Log.Info(f"Processing Molru \"{Path}{File}\"...");
 					match Extractor.lower():
-						case "slow": Extract_Slow(f"{Path}{File}", Path);
-						case "deep": Extract_Deep(f"{Path}{File}", Path);
+						case "slow": Extract_Slow(f"{Path}{File}");
+						case "deep": Extract_Deep(f"{Path}{File}");
 						case _: raise Exception(f"Unknown Extractor: {Extractor}");
 					Log.Awaited().OK();
 
