5cce3524e111deb0535fa8da320c58414a127cdf
Ascellayn <ascellayn.sagittarii+github@sirio-network.com>
2026-04-10 16:35:16 +0200

    R¤MXMC | #7 << Implement DRASTICALLY FASTER MXMC Caching Algorithm

--- TSN_Kozeki.py
+++ TSN_Kozeki.py
@@ -1,9 +1,9 @@
 from TSN_Abstracter import *;
 import re, sys, typing;
 
-Log.Clear();
-Kozeki_Version: str = "v0.8.2";
-Kozeki_Branch: str = "Azure";
+
+
+
 
 Limit_Logs: bool = False;
 
@@ -11,69 +11,85 @@ MXMC_Only: bool = False;
 MXMC_Disabled: bool = False;
 MXMC_Dictionary: dict[str, list[tuple[str, str, str, str]]] = {};
 
+
+
+
+
 def MX_MediaCatalog() -> None:
 	""" The MX Media Catalogue (MXMC) is a .bytes file containing the internal name of a file and its path.
-	We use the MXMC to rename our extracted files to their actual names. This function is currently slow and will be rewritten to maybe use Regex in the future for blazing fast processing."""
+	We use the MXMC to rename our extracted files to their actual names.
+	
+	Huge credits to @Apis035 for making me realize a SIGNIFICANTLY better way of handling MXMC."""
 	if (MXMC_Disabled): return;
 
 	# Hardcoded path for now because quite frankly the only file we need to deal with at the moment.
 	global MXMC_Dictionary;
 
 	MXMC_Init: int = Time.Get_Unix(True); MXMC_Progress: int = MXMC_Init;
-	Buffer_Internal: bytes = b""; Buffer_External: bytes = b""; Key: str;
-	Internal: bool = True;
+	Key: str;
 
 	with open("BlueArchive_Data/StreamingAssets/PUB/Resource/Catalog/MediaResources/MediaCatalog.bytes", "r+b") as NCB: MXMC_Data: bytes = NCB.read();
 
-	MXMC_Dictionary["__Version"] = MXMC_Data[1] + MXMC_Data[2]; # pyright: ignore[reportArgumentType] // stfu
-	MXMC_Data = MXMC_Data[13:]; # We don't care about the header nor the "Internal/Unknown" 64 bits value.
+	Log.Stateless(f"Calculating MXMC Hash...");
+	MXMC_Dictionary["__Hash"] = hashlib.md5(MXMC_Data, usedforsecurity=False).hexdigest(); # pyright: ignore[reportArgumentType]
+	Log.Awaited().OK();
+
+	MXMC_Dictionary["__Size"] = int.from_bytes(MXMC_Data[1:4],byteorder="little"); # pyright: ignore[reportArgumentType] // stfu
+	MXMC_Data = MXMC_Data[5:]; # Skip MXMC Header
 	MXMC_Total: int = len(MXMC_Data);
 
-	Log.Info(f"Need to process a total of {len(MXMC_Data)} Bytes; MXMC Version \"{MXMC_Dictionary["__Version"]}\" ");
+	Log.Info(f"Need to process a total of {len(MXMC_Data)} Bytes; MXMC Hash: \"{MXMC_Dictionary["__Hash"]}\" - Size: \"{MXMC_Dictionary["__Size"]}\" ");
 
 	if (File.Exists("MXMC_Definitions.cjson")):
 		MXMC_Cached: dict[str, Any] = File.JSON_Read("MXMC_Definitions.cjson", True);
-		if (MXMC_Cached["__Version"] == MXMC_Dictionary["__Version"]):
-			Log.Info(f"Cached MXMC Definitions Cache found and versions match, avoiding re-discovering everything.");
-			MXMC_Dictionary = MXMC_Cached;
-			return;
-		else: Log.Warning(f"An MXMC Definitions Cache was found but is outdated! Will need to rediscover... (Expected {MXMC_Dictionary["__Version"]}, got {MXMC_Cached["__Version"]})");
+		if ("__Hash" in MXMC_Cached.keys()):
+			if (MXMC_Cached["__Hash"] == MXMC_Dictionary["__Hash"]):
+				Log.Info(f"Cached MXMC Definitions Cache found and versions match, avoiding re-discovering everything.");
+				MXMC_Dictionary = MXMC_Cached;
+				return;
+			else: Log.Warning(f"An MXMC Definitions Cache was found but is outdated! Will need to rediscover... (Expected {MXMC_Dictionary["__Hash"]}, got {MXMC_Cached["__Hash"]})");
+		else: Log.Warning(f"Pre-{App.Name} v0.8.3 MXMC Cache found, the cache will need to be re-discovered to properly make sure it is up to date!");
 	else: Log.Warning(f"No MXMC Definitions Cache was found, discovering file names...");
-	Log.Stateless("Kozeki will now attempt to parse every single Filename that Blue Archive uses inside the BlueArchive_Data/StreamingAssets/ folder.\nThis is used so that exported data from Molru files have proper file names instead of their raw hex positions. This will take unfortunately a while, please be patient.");
+	Log.Stateless("Kozeki will now attempt to parse every single Filename that Blue Archive uses inside the BlueArchive_Data/StreamingAssets/ folder.\nThis is used so that exported data from Molru files have proper file names instead of their raw hex positions.");
 
-	while (True):
-		Byte: bytes = MXMC_Data[0:1];
-		if (Byte in [b"\x03", b"\x02", b"\x01"]): # 0x03 = GameData // 0x02 = Preload // 0x01 = Root
-			MXMC_Data = MXMC_Data[9:];
-			if (Internal): Internal = False; continue;
+	Entries_Processed: int = 0;
+	while (Entries_Processed < MXMC_Dictionary["__Size"]):
+		# First u32 is currently unknown
+			# Internal In-Game Name
+		iLength: int = int.from_bytes(MXMC_Data[4:8], byteorder="little") + 8;
+		iName: str = str(MXMC_Data[8:iLength], "ASCII");
 
-			match Byte:
-				case b"\x03": Key_Pre: str = "BlueArchive_Data/StreamingAssets/PUB/Resource/GameData/MediaResources/";
-				case b"\x02": Key_Pre: str = "BlueArchive_Data/StreamingAssets/PUB/Resource/Preload/MediaResources/";
-				case b"\x01": Key_Pre: str = "BlueArchive_Data/StreamingAssets/";
-				case _:
-					Log.Critical(f"John Nexon added a new Folder ID to the MXMC File Format, please notify Ascellayn to go coin himself in THE FINALS.\nKozeki will now close to prevent bad extractions. And by close we mean crashin-");
-					raise Exception(Byte);
+			# Physical Location
+		pLength: int = int.from_bytes(MXMC_Data[iLength+5:iLength+9], byteorder="little") + 8;
+		pName: str = str(MXMC_Data[iLength+9:iLength+pLength+1], "ASCII");
+		Content_Folder: bytes = MXMC_Data[iLength+pLength+1:iLength+pLength+2];
 
-			Key = Key_Pre + "/".join(str(Buffer_External, "ASCII").split("/")[:-1]);
-			if (Key not in MXMC_Dictionary.keys()): MXMC_Dictionary[Key] = [];
-			MXMC_Dictionary[Key].append((str(Buffer_Internal, "ASCII"), str(Buffer_External, "ASCII"), str(Buffer_Internal, "ASCII").split("/")[-1], str(Buffer_External, "ASCII").split("/")[-1]));
-			Buffer_External = b""; Buffer_Internal = b""; Internal = True;
+		match Content_Folder:
+			case b"\x03": Key_Pre: str = "BlueArchive_Data/StreamingAssets/PUB/Resource/GameData/MediaResources/";
+			case b"\x02": Key_Pre: str = "BlueArchive_Data/StreamingAssets/PUB/Resource/Preload/MediaResources/";
+			case b"\x01": Key_Pre: str = "BlueArchive_Data/StreamingAssets/";
+			case _:
+				Log.Critical(f"John Nexon added a new Folder ID to the MXMC File Format, please notify Ascellayn to go coin himself in THE FINALS.\nKozeki will now close to prevent bad extractions. And by close we mean crashin-");
+				raise Exception(Content_Folder);
+		
+		Key = Key_Pre + "/".join(pName.split("/")[:-1]);
+		if (Key not in MXMC_Dictionary.keys()): MXMC_Dictionary[Key] = [];
+		MXMC_Dictionary[Key].append((
+			iName,
+			pName,
+			iName.split("/")[-1],
+			pName.split("/")[-1]
+		));
+		Log.Debug(f"Content Type: {Content_Folder} | Internal: char[{iLength}] \"{iName}\" | Physical: char[{pLength}] \"{pName}\"");
 
-			Log.Debug(f"Discovered {MXMC_Dictionary[Key][-1][3]} in {Key} // {len(MXMC_Data)} Bytes left to process");
-			if (len(MXMC_Data) < 4): break;
-			MXMC_Data = MXMC_Data[7:];
+		if ((Time.Get_Unix() - MXMC_Progress) > 1):
+			Log.Carriage(f"MX Catalog Parsing → {MXMC_Total - len(MXMC_Data)}/{MXMC_Total} ({round(((MXMC_Total - len(MXMC_Data))/MXMC_Total)*100, 2)}%) Bytes Processed");
+			MXMC_Progress = Time.Get_Unix();
 
-			if ((Time.Get_Unix() - MXMC_Progress) > 1):
-				Log.Carriage(f"MX Catalog Parsing → {MXMC_Total - len(MXMC_Data)}/{MXMC_Total} ({round(((MXMC_Total - len(MXMC_Data))/MXMC_Total)*100, 2)}%) Bytes Processed");
-				MXMC_Progress = Time.Get_Unix();
+		MXMC_Data = MXMC_Data[iLength+pLength+9:];
+		Entries_Processed += 1;
 
-
-		else:
-			if (Internal): Buffer_Internal += Byte;
-			else: Buffer_External += Byte;
-			MXMC_Data = MXMC_Data[1:];
-	Log.Awaited().Status_Update(f"[OK: {len(MXMC_Dictionary.keys())} File Definitions Found]")
+	Log.Awaited().OK(f"{len(MXMC_Dictionary.keys())} Folder Definitions Found")
 
 	Log.Info(f"Writing Cached MXMC cJSON...");
 	File.JSON_Write("MXMC_Definitions.cjson", MXMC_Dictionary, True);
@@ -292,25 +308,21 @@ def Help():
 
 if (__name__ == '__main__'):
 	global Debug_Mode; Debug_Mode: bool;
+	App.Name = "Kozeki";
+	App.Description = "Kozeki is a TSNA based tool to extract Blue Archive's .molru PC files, a cursed file type given to us who like to poke around a bit too much.";
+	App.Author = ["Ascellayn", "The Sirio Network"];
+	App.License = "TSN License 2.1 - Universal";
+	App.License_Year = "2025-2026";
+	App.Codename = "TSN_Kozeki";
+	App.Branch = "Azure";
+	App.Version = (0,8,3);
+	App.TSNA = (6,0,0);
 
-	if (TSN_Abstracter.Version_Tuple[0] >= 6):
-		# TSNA "Azure" v6+ Compatibility and avoid making users redownload the App.tsna file
-		App.Name = "Kozeki";
-		App.Description = "Kozeki is a TSNA based tool to extract Blue Archive's .molru PC files, a cursed file type given to us who like to poke around a bit too much.";
-		App.Author = ["Ascellayn", "The Sirio Network"];
-		App.License = "TSN License 2.1 - Universal";
-		App.License_Year = "2025-2026";
-		App.Codename = "TSN_Kozeki";
-		App.Branch = "Azure";
-		App.Version = (6,0,0);
-		App.TSNA = (6,0,0);
+	TSN_Abstracter.App_Init(False);
 
-		TSN_Abstracter.App_Init(False);
-	else:
-		Log.Stateless(f"Kozeki {Kozeki_Branch} - {Kozeki_Version} © Ascellayn / The Sirio Network (2025-2026) | TSN License 2.1 - Universal");
-		Log.Stateless("Kozeki is a TSNA based tool to extract Blue Archive's .molru PC files, a cursed file type given to us who like to poke around a bit too much.\n");
-		TSN_Abstracter.Require_Version((5,5,0));
-		Log.Warning(f"You seem to be using TSN Abstracter 'Everest' (v5.X.X), Kozeki will still function just fine but do note that in the future you may need to download a newer version of TSNA if Kozeki ever needs a newer function from it.");
+
+
+
 
 	# Argument Configuration
 	Extractor: str = "regex";
@@ -352,5 +364,5 @@ if (__name__ == '__main__'):
 		else: Kozeki_Repacker(Repack_Folder);
 	else: File.JSON_Write("MXMC_Definitions.json", File.JSON_Read("MXMC_Definitions.cjson", True), False);
 
-else: TSN_Abstracter.Require_Version((5,5,0));
+else: TSN_Abstracter.Require_Version((6,0,0));
 # ↑ In case someone wants to import this file and use its extractors outside of the Kozeki script.
